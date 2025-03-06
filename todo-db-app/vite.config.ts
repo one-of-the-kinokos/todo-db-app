ad6a95088e8f124b34f7c94251e12b6b792c02fb
KINOKO_MAN <ogaki.viator.masashi@gmail.com>
2025-03-06 11:29:05 +0900

    viteが404.htmlも含めてビルドするように設定を変更。

--- todo-db-app/vite.config.ts
+++ todo-db-app/vite.config.ts
@@ -2,11 +2,20 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig((configObj) => {
   return {
     base: configObj.mode === "gh-pages" ? "/todo-deb-app" : "/",
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          404: resolve(__dirname, "404.html"),
+        },
+      },
+    },
     plugins: [
       TanStackRouterVite({
         target: "react",
